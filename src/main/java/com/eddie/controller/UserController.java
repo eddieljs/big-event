5a15dd23ec4554708167305f0311344d6fb90f49
eddieljs <2574350328.com>
2024-09-08 19:48:11 +0800

    用户登录

--- src/main/java/com/eddie/controller/UserController.java
+++ src/main/java/com/eddie/controller/UserController.java
@@ -4,6 +4,7 @@ package com.eddie.controller;
 import com.eddie.pojo.Result;
 import com.eddie.pojo.User;
 import com.eddie.service.Userservice;
+import com.eddie.utils.Md5Util;
 import jakarta.validation.constraints.Pattern;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,13 @@ public class UserController {
 
     @Autowired
     private Userservice userservice;
+
+    /**
+     * 用户注册
+     * @param username
+     * @param password
+     * @return
+     */
     @PostMapping("/register")
     public Result register(@Pattern(regexp = "^\\S{5,16}$") String username, @Pattern(regexp = "^\\S{5,16}$")String password){
         log.info("用户注册：{}",username);
@@ -33,6 +41,25 @@ public class UserController {
         }else {
             return Result.success("用户名已存在");
         }
+
+    }
+
+    @PostMapping("/login")
+    public Result login(@Pattern(regexp = "^\\S{5,16}$") String username, @Pattern(regexp = "^\\S{5,16}$")String password) {
+        log.info("用户登录：{}", username);
+        //根据用户名查User
+        User user = userservice.getByUsername(username);
+        //判断是否查到
+        if(user==null){
+            return Result.error("用户不存在");
+        }
+        String md5String = Md5Util.getMD5String(password);
+        //判断密码是否正确
+        if (md5String.equals(user.getPassword()) ){
+            return Result.success("jwt");
+        }
+            return Result.error("密码错误");
+
 
     }
 }
